af237586cf012f1f15387516c7ca7e00a6a82598
Ahmed El-Essawy <ahm.elessawy@gmail.com>
2017-03-10 15:12:41 +0200

    Build ASP Client

---
+++ Project_Client_ASP.NET/index.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="index.aspx.cs" Inherits="index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:DropDownList runat="server"></asp:DropDownList>
+    </form>
+</body>
+</html>
---
+++ Project_Client_ASP.NET/index.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class index : System.Web.UI.Page
+{
+    private WebClient client;
+    0
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        client = new WebClient();
+        string response = client.DownloadString(new Uri("http://localhost:83/Proj.svc/Proj/REST/Employees"));
+        emplist = JsonConvert.DeserializeObject<Employee[]>(response);
+        foreach (Employee emp in emplist)
+            comboBox_Emp.Items.Add(emp.Name);
+        FillForm(0);
+    }
+}
